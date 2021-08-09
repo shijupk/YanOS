e0b702ac5c5cdcb01dcd13da125a93dd3ef9f6f0
Shiju P K <Shiju.PK@philips.com>
2021-08-09 23:07:55 +0530

    fixed build errors

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,6 +3,7 @@
 void yanos_main()
 {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to Yanos x64!!!");
+    print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
+    print_str("\n--------------------  Yanos x64!!! ------------------------\n");
+    print_str("\n-------------------- Nothing so far ------------------------\n");
 }
